2de458b4e0cd80eed2b59a728767554a8b5f3c32
rabbia487 <rabbiaamir487@gmail.com>
2026-03-23 19:51:20 +0200

    task 4

---
+++ lecture 1/Qno4.py
@@ -0,0 +1,9 @@
+value1 = float(input("enter value 1:"))
+value2 = float(input("enter value 2:"))
+value3 = float(input("enter value 3:"))
+sum = value1+value2+value3
+product = value1*value2*value3
+avarage = sum/3
+print("product is ", product)
+print("sum is", sum)
+print ("avarage is", avarage)
