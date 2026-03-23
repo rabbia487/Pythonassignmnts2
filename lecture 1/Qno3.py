0ae6dc6d2509ee85ee736ce6d5480d68f8a8bbcb
rabbia487 <rabbiaamir487@gmail.com>
2026-03-23 19:36:26 +0200

    task 3

---
+++ lecture 1/Qno3.py
@@ -0,0 +1,6 @@
+l = float(input("enter length of the rectangle:"))
+w = float(input("enter width of the rectangle:"))
+perimeter =2*(l+w)
+area = l*w
+print("area of rectangle is", area)
+print ("perimeter of rectangle is", perimeter)
