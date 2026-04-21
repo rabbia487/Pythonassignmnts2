c696264bcbac263501f468a3dfe3c76680747c4f
rabbia487 <rabbiaamir487@gmail.com>
2026-04-21 17:29:09 +0300

    Task4

---
+++ lecture 4/QNO4.py
@@ -0,0 +1,10 @@
+cities = []
+
+# Ask for 5 cities
+for i in range(5):
+    name = input("Enter a city: ")
+    cities.append(name)
+
+# Print cities one by one
+for city in cities:
+    print(city)
