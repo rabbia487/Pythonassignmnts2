153d9dd500e5369e391c65c57cdae4c60283f7ed
rabbia487 <rabbiaamir487@gmail.com>
2026-05-11 11:40:13 +0300

    qno3

---
+++ lecture 7/qno3.py
@@ -0,0 +1,40 @@
+class Car:
+
+    def __init__(self, registration_number, maximum_speed):
+
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+
+    def accelerate(self, change_of_speed):
+
+        self.current_speed += change_of_speed
+
+
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+
+
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+
+    def drive(self, hours):
+
+        self.travelled_distance += self.current_speed * hours
+
+
+
+car1 = Car("ABC-123", 142)
+
+
+car1.current_speed = 60
+car1.travelled_distance = 2000
+
+
+car1.drive(1.5)
+
+
+print("Travelled Distance:", car1.travelled_distance)
